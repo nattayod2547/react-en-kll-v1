8db5c1189840685649f1628fd2a342aca09bb276
nattayod2547 <83393958+nattayod2547@users.noreply.github.com>
2024-08-10 16:10:38 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbarr from './components/Navbarr'
 import Boxboyde from './components/Boxboyde'
+import Aboutpag from './components/Aboutpag'
 
 function App() {
 
@@ -11,7 +12,9 @@ function App() {
     <Navbarr/>
       <div className='mt-16'>
         <Boxboyde/>
-        <p className='h-screen'>lk;sad';l</p>
+        <Aboutpag/>
+        
+        
         <p className='h-screen'>lk;sad';l</p>
         <p className='h-screen'>lk;sad';l</p>
         <p className='h-screen'>lk;sad';l</p>
---
+++ src/components/Aboutpag.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Aboutpag = () => {
+  return (
+    <>
+    <div className='h-[750px] w-full '>
+        {/* <div className='' > */}
+            <div className='w-full grid justify-center text-shadow-gradient-shadow'>
+                <h1 className='text-[50px] font-semibold bg-custom-gradient text-transparent bg-clip-text'>About Me</h1>
+            </div>
+            <div className='w-full  grid justify-center mt-[100px]'>
+                <p className='w-[300px] h-full  sm:w-[600px] text-slate-600 font-medium'>
+                    Hello! I’m Nattayod Lamsan, and I’m currently diving into the world of web development. I’m learning React, 
+                    TailwindCSS, and Node.js to build dynamic and visually appealing web applications. My journey into tech started 
+                    with a background in automotive engineering, having graduated with a Vocational Certificate in Automotive Technology 
+                    from Techno Lopburi. My passion for technology and problem-solving drives me to continuously learn and improve my skills. 
+                    I’m excited about the opportunities ahead and am eager to apply my knowledge in creating innovative solutions.
+                </p>
+            </div>
+
+        {/* </div> */}
+    </div>
+
+
+    </>
+  )
+}
+
+export default Aboutpag
